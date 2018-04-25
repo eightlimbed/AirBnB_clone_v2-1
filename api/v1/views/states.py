3a068f8af0d8bb778c85e307cd34ffde3eb474cc
Lee Gaines <eightlimbed@gmail.com>
2018-04-25 13:30:54 -0700

    adds POST route

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,25 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
 from models import storage
-from flask import jsonify, abort
+from models.state import State
+from flask import jsonify, abort, request
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def create_state():
+    '''
+    Creates a state if state_id matches a state in storage
+    '''
+    if not request.json:
+        abort(400, "Not a JSON")
+    state_json = request.get_json()
+    if 'name' not in state_json.keys():
+        abort(400, "Missing name")
+    new_state = State()
+    for key, val in state_json.items():
+        new_state.__dict__[key] = val
+
+    return jsonify(new_state.to_dict()), 201
 
 
 @app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
