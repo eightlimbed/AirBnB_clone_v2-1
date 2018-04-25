3cfef8d3f27e4463924dad12ec78ea244a374d52
Lee Gaines <eightlimbed@gmail.com>
2018-04-25 05:31:38 -0700

    adds stats route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
+from models import storage
 from flask import jsonify
 
 
 @app_views.route('/status')
 def status():
     return jsonify({'status': 'OK'})
+
+@app_views.route('/stats')
+def stats():
+    stats = {
+        'amenities': storage.count('Amenity')
+    }
+    return jsonify({'stats': stats})
