11941f35ee0cd9560eb59331fdda6e43ff8499c2
Lee Gaines <eightlimbed@gmail.com>
2018-04-25 11:35:57 -0700

    adds states by id GET route

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
 from models import storage
-from flask import jsonify
+from flask import jsonify, abort
 
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def state(state_id):
+    '''
+    Retrieves json representation of a state if it exists
+    '''
+    for key, obj in storage.all('State').items():
+        if obj.id == state_id:
+            return jsonify(obj.to_dict())
+    abort(404)
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def states():
@@ -13,3 +22,4 @@ def states():
     for key, obj in storage.all('State').items():
         states[key] = obj.to_dict()
     return jsonify(states)
+
