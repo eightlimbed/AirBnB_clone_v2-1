9318a776035678e9531561a966e048e57048d8a0
Lee Gaines <eightlimbed@gmail.com>
2018-04-25 14:46:03 -0700

    adds PUT route

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -5,6 +5,24 @@ from models.state import State
 from flask import jsonify, abort, request
 
 
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def update_state(state_id):
+    '''
+    Updates a state if it exists
+    '''
+    if not request.json:
+        abort(400, "Not a JSON")
+    state = storage.get('State', state_id)
+    if state is None:
+        abort(404)
+    new_state = request.get_json()
+    for key, val in new_state.items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(state, key, val)
+    storage.save()
+    return jsonify(state.to_dict()), 200
+
+
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
     '''
@@ -16,13 +34,15 @@ def create_state():
     if 'name' not in state_json.keys():
         abort(400, "Missing name")
     new_state = State()
+    storage.new(new_state)
     for key, val in state_json.items():
         new_state.__dict__[key] = val
-
+    storage.save()
     return jsonify(new_state.to_dict()), 201
 
 
-@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_state(state_id):
     '''
     Deletes a state if state_id matches a state in storage
@@ -54,4 +74,3 @@ def show_states():
     for key, obj in storage.all('State').items():
         states[key] = obj.to_dict()
     return jsonify(states)
-
