2667a8e3ba3714c94b408e981d13a7f1d982d818
Lee Gaines <eightlimbed@gmail.com>
2018-04-25 11:40:44 -0700

    adds DELETE route

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,8 +3,21 @@ from api.v1.views import app_views
 from models import storage
 from flask import jsonify, abort
 
+
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+def delete_state(state_id):
+    '''
+    Deletes a state if state_id matches a state in storage
+    '''
+    for key, obj in storage.all('State').items():
+        if obj.id == state_id:
+            storage.delete(obj)
+            return jsonify({})
+    abort(404)
+
+
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
-def state(state_id):
+def get_state(state_id):
     '''
     Retrieves json representation of a state if it exists
     '''
@@ -13,8 +26,9 @@ def state(state_id):
             return jsonify(obj.to_dict())
     abort(404)
 
+
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
-def states():
+def show_states():
     '''
     Retrieves the list of all State objects
     '''
